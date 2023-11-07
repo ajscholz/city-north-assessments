d125bdd9d3f98dac0c6be765f734ec415abb6595
ajscholz <yoitsscholzy@gmail.com>
2023-11-07 13:31:53 -0500

    add & remove necessary deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
 module.exports = {
   siteMetadata: {
     title: `City North Assessments`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://city-north-assessments.netlify.app`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
-};
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/Layout.js`),
+      },
+    },
+  ],
+}
