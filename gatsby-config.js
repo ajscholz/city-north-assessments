79a63d0d251cebd6f0ed7bdd8ca7d770ead48f32
ajscholz <yoitsscholzy@gmail.com>
2023-11-07 13:26:05 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `City North Assessments`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+};
